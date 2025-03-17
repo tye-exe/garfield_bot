15a5f4b9b3d08448dda2b4fb04659a3c1bf4d1cc
tye-exe <tye@mailbox.org>
2025-03-17 23:34:11 +0000

    Reverted to known good comic source

--- src/lib.rs
+++ src/lib.rs
@@ -2,45 +2,42 @@ pub mod old_commands;
 
 use anyhow::anyhow;
 use chrono::NaiveDate;
-use scraper::{Html, Selector};
+use scraper::{Element, Html, Selector};
 
 pub async fn get_comic(comic_date: NaiveDate) -> anyhow::Result<String> {
     // Format current date
-    let time = comic_date.format("%Y%m%d");
+    let time = comic_date.format("%Y/%m/%d");
 
     // Get html data of garfield data for current day
-    let text = reqwest::get(format!("https://www.mezzacotta.net/garfield/?date={}", time))
+    let text = reqwest::get(format!("https://www.gocomics.com/garfield/{}", time))
         .await?
+        .error_for_status()?
         .text()
         .await?;
 
     // Set up parsing data structures
     let html_data = Html::parse_document(&text);
-    let selector = Selector::parse("p img").map_err(|_| anyhow!("Cannot parse 'p img' from the returned HTML"))?;
+    let selector =
+        Selector::parse("div").map_err(|_| anyhow!("Cannot parse 'div' from the returned html"))?;
 
     // Parse comic image url from html data
     let comic_url = html_data
         .select(&selector)
+        .into_iter()
+        .filter(|element| {
+            // Remove everything other than the comic
+            element.has_class(
+                &("comic".into()),
+                scraper::CaseSensitivity::AsciiCaseInsensitive,
+            )
+        })
+        .map(|element| {
+            element
+                .attr("data-image")
+                .ok_or(anyhow!("Cannot parse 'data-image' from comic strip"))
+        })
         .next()
-        .ok_or(anyhow!("Could not find comic image in HTML"))?
-        .value()
-        .attr("src")
-        .ok_or(anyhow!("Could not extract 'src' attribute from comic image"))?;
+        .ok_or(anyhow!("Could not parse comic image url from html data"))??;
 
-    let full_url = format!("https://www.mezzacotta.net{}", comic_url);
-    Ok(full_url)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[tokio::test]
-    async fn test_get_comic() {
-        let today = chrono::Utc::now().naive_utc().date();
-        match get_comic(today).await {
-            Ok(img_url) => println!("Latest comic image URL: {}", img_url),
-            Err(e) => eprintln!("Error fetching comic: {}", e),
-        }
-    }
+    Ok(comic_url.to_owned())
 }
